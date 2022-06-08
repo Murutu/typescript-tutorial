e9183ccfc257191c9660a53efb653d6523ce13f1
Peter Murutu <murutupeter@gmail.com>
2022-06-08 16:16:38 +0300

    set up

---
+++ src/index.ts
@@ -0,0 +1,17 @@
+/* 
+Typescript is a programming language  created by microsoft 
+to address shortcomings of javascript.
+Typescript helps us build more robust & maintainable applications in less time.
+Is javascript with type checking.
+Is a superset of javascript meaning it has everything of javascript + other features.
+
+**BENEFITS**
+Static typing , Code completion , Refactoring , Shorthand notations
+
+**DRAWBACKS**
+Compilation, Discipline in Coding. 
+
+Is a programming language built on top of javascript. So every javascript file is a valid typescript file
+*/
+
+let age: number = 20;
